502eb97023e82470d87c3b456f5a78c751c5732b
Jeff Hartman <applejeff@mac.com>
2018-06-26 14:17:32 -0500

    working through setting up models in lesson 3

--- models/user.js
+++ models/user.js
@@ -15,7 +15,7 @@ var UserSchema = mongoose.Schema({
 		type: String,
 	},
 	email: {
-		type: String
+		type: String  
 	},
 	name: {
 		type: String
@@ -33,3 +33,19 @@ module.exports.createUser = function (newUser, callback){
     	});
 	});
 };
+
+module.exports.getUserByUsername = function (username, callback) {
+	var query = {username: username};
+	User.findOne(query, callback);
+}
+
+module.exports.getUserById = function (id, callback) {
+	User.findById(id, callback);
+}
+
+module.exports.comparePassword = function (candidatePassword, hash, callback) {
+	bcrypt.compare(candidatePassword, hash, function (err, isMatch) {
+		if (err) throw err;
+		callback (null, isMatch);
+	})
+}
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
 var User = require('../models/user');
 
@@ -54,6 +56,46 @@ router.post('/register', function(req, res) {
 		res.redirect('/users/login');
 	}
 });
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+  	User.getUserByUsername(username, function(err, user) {
+  		if (err) throw err;
+  		if (!user) {
+  			return done (null, false, {message: 'Invalid user/password combination'});
+  		}
+
+  		User.comparePassword(password, user.password, function(err, isMatch) {
+  			if (err) throw err;
+  			if (isMatch) {
+  				return done(null, user);
+  			} else {
+  				return done (null, false, {message: 'Invalid user/password combination'});
+  			}
+  		});
+  	});
+  }));
+
+passport.serializeUser(function (user, done) {
+	done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+	User.getUserById(id, function(err, user){
+		done(err, user);
+	})
+});
+
+router.post('/login', 
+	passport.authenticate('local', 
+	{
+		successRedirect: '/',
+		failureRedirect: '/users/login',
+		failureFlash: true
+	}),
+	function(req, res) {
+		res.redirect('/');
+  });
 
 
 
